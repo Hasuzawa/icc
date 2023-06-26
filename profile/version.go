2e66ffca1e835d641477b444b9908158ed6ea862
Hasuzawa <hasuzawa.yuichi.international@gmail.com>
2023-06-26 23:30:59 +0900

    add: version info

---
+++ profile/version.go
@@ -0,0 +1,17 @@
+package profile
+
+var Module = struct {
+	Name        string
+	Version     string
+	Description string
+	Homepage    string
+	Author      string
+	License     string
+}{
+	Name:        "icc",
+	Version:     "v0.1.0",
+	Description: "Go module for reading and decoding ICC profile",
+	Homepage:    "https://github.com/Hasuzawa/icc",
+	Author:      "Hasuzawa",
+	License:     "BSD-3-Clause",
+}
